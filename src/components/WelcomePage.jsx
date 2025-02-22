b87e849e584bfd2048b8a7c5ad5c1495028b04e7
Satyam Singh <satyamrajput7239@gmail.com>
2025-02-22 08:55:06 +0530

    feat:Employee Login/Register

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import "./WelcomePage.css";
 const WelcomePage = () => {
   const [userSelect, setUserSelect] = useState(0);
-  const handlerAdmin = () => {};
+  const [newEmployee, setNewEmployee] = useState(true);
+  const handlerAdminLogin = () => {};
+  const handlerEmployeeLogin = () => {};
   return (
     <div className="wel-main-container">
       <nav className="wel-nav">
@@ -35,20 +37,106 @@ const WelcomePage = () => {
         </div>
       </nav>
       <main className="wel-form-container">
-        <h2 className="form-title">Admin Login</h2>
-        <form onSubmit={handlerAdmin} className="admin-login-form">
-          <label className="form-label">User Name :</label>
-          <input type="text" placeholder="user name" className="form-input" />
-          <label className="form-label">Password :</label>
-          <input
-            type="password"
-            placeholder="password"
-            className="form-input"
-          />
-          <button className="wel-nav-btn form-submit-btn" type="submit">
-            Submit
-          </button>
-        </form>
+        {userSelect === 1 && (
+          <>
+            <h2 className="form-title">Admin Login</h2>
+            <form onSubmit={handlerAdminLogin} className="admin-login-form">
+              <label className="form-label">User Name :</label>
+              <input
+                type="text"
+                placeholder="user name"
+                className="form-input"
+              />
+              <label className="form-label">Password :</label>
+              <input
+                type="password"
+                placeholder="password"
+                className="form-input"
+              />
+              <button className="wel-nav-btn form-submit-btn" type="submit">
+                Submit
+              </button>
+            </form>
+          </>
+        )}
+        {userSelect === 2 &&
+          (newEmployee ? (
+            <>
+              <h2 className="form-title">Employee Login</h2>
+              <form
+                onSubmit={handlerEmployeeLogin}
+                className="admin-login-form"
+              >
+                <label className="form-label">User Name :</label>
+                <input
+                  type="text"
+                  placeholder="user name"
+                  className="form-input"
+                />
+                <label className="form-label">Password :</label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="form-input"
+                />
+                <button className="wel-nav-btn form-submit-btn" type="submit">
+                  Submit
+                </button>
+                <p className="form-employee-text">
+                  New Register?
+                  <span
+                    className="form-employee-text-span"
+                    onClick={() => {
+                      setNewEmployee(false);
+                    }}
+                  >
+                    Register
+                  </span>
+                </p>
+              </form>
+            </>
+          ) : (
+            <>
+              <h2 className="form-title">Employee Register</h2>
+              <form
+                onSubmit={handlerEmployeeLogin}
+                className="admin-login-form"
+              >
+                <label className="form-label">User Name :</label>
+                <input
+                  type="text"
+                  placeholder="user name"
+                  className="form-input"
+                />
+                <label className="form-label">Password :</label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="form-input"
+                />
+                <label className="form-label">Confirm Password :</label>
+                <input
+                  type="password"
+                  placeholder="confirm password"
+                  className="form-input"
+                />
+                <button className="wel-nav-btn form-submit-btn" type="submit">
+                  Submit
+                </button>
+                <p className="form-employee-text">
+                  Already register?
+                  <span
+                    className="form-employee-text-span"
+                    onClick={() => {
+                      setNewEmployee(true);
+                    }}
+                  >
+                    Login
+                  </span>
+                </p>
+              </form>
+            </>
+          ))}
       </main>
     </div>
   );
